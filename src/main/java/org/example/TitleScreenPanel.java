1cb27fecf5ee8e64635d0f4b16f8a4958cf10c4b
Ali Tahboub <tahboub.ali34@gmail.com>
2025-01-08 11:16:37 +0300

    added key listener so you can press enter instead of pressing play.

--- src/main/java/org/example/TitleScreenPanel.java
+++ src/main/java/org/example/TitleScreenPanel.java
@@ -2,14 +2,18 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Map;
 
 public class TitleScreenPanel extends JPanel {
-    private final int WIDTH = 500, HEIGHT = 800;
+    private final int WIDTH = 500;
     private String username;
-
+    private final JTextField text;
 
     public TitleScreenPanel() {
+        int HEIGHT = 800;
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setLayout(null);
         var label = new JLabel("Enter a username");
@@ -17,22 +21,31 @@ public class TitleScreenPanel extends JPanel {
         var bounds = getFontMetrics(label.getFont()).getStringBounds("Enter a username:", getGraphics());
         label.setBounds(500 / 2 - (int) bounds.getWidth() / 2, 300, (int) bounds.getWidth(), (int) bounds.getHeight());
         add(label);
-        var text = new JTextField();
+        text = new JTextField();
         text.setFont(new Font(getFont().getFontName(), getFont().getStyle(), 15));
         text.setBounds(label.getX(), label.getY() + 60, (int) bounds.getWidth(), 40);
+        text.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER)
+                    submit();
+            }
+        });
         add(text);
+
         var submitBtn = new JButton("PLAY!");
         var btnBounds = new Rectangle(label.getX(), text.getY() + 90, (int) bounds.getWidth(), (int) bounds.getWidth());
         btnBounds.grow(20, 20);
         submitBtn.setBounds(btnBounds);
         submitBtn.setFont(new Font(getFont().getFontName(), getFont().getStyle(), 48));
-        submitBtn.addActionListener(_ -> {
-            if (!text.getText().isBlank()) {
-                setUsername(text.getText());
-            }
-        });
+        submitBtn.addActionListener(_ -> submit());
         submitBtn.setFocusable(true);
         add(submitBtn);
+    }
+
+    private void submit() {
+        if (!text.getText().isBlank())
+            setUsername(text.getText());
     }
 
     @Override
