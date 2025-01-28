955e365a248fbb50f7619c8da31553a1940c726d
Ali Tahboub <tahboub.ali34@gmail.com>
2025-01-28 10:45:49 +0300

    added restrictions in setters for positions in blocks and players, design needs rethinking

---
+++ src/main/java/org/example/DuelManager.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class DuelManager {
+    private ConnectionHandler connectionHandler;
+
+    public DuelManager(ConnectionHandler connectionHandler) {
+        this.connectionHandler = connectionHandler;
+    }
+}
