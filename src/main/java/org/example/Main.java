35c55ac299b4a0cdbb22283a6a966f84f6c925f8
Ali Tahboub <tahboub.ali34@gmail.com>
2025-02-20 11:58:37 +0300

    added yo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,7 @@ package org.example;
 
 import javax.swing.*;
 
-import java.awt.*;
-
 import static java.lang.Thread.startVirtualThread;
-import static org.example.PolyUtils.from;
-import static org.example.PolyUtils.getCorners;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,9 +10,10 @@ public class Main {
     }
 
     private static void run() {
+        // yooo
         var window = new JFrame();
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//        window.setUndecorated(true);
+        window.setUndecorated(true);
         var game = new GamePanel();
         window.add(game);
         window.pack();
