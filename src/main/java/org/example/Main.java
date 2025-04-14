e9624f120bec139c3d0ffecd5ce805bf6a1b7e63
Ali Tahboub <tahboub.ali34@gmail.com>
2025-04-14 10:16:49 +0300

    fixed minor issue in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,6 +23,7 @@ public class Main {
         var game = new GamePanel();
         game.setSize(window.getSize());
         game.setLocation(0, 0);
+
         layeredPane.add(game, JLayeredPane.DEFAULT_LAYER);
         game.createSidePanel();
         window.setContentPane(layeredPane);
@@ -42,3 +43,47 @@ public class Main {
         Main.username = username;
     }
 }
+
+
+/*
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.lang.Thread.startVirtualThread;
+
+public class Main {
+    public static void main(String[] args) {
+        run();
+    }
+
+    private static void run() {
+        var window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        window.setUndecorated(true);
+        var layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(window.getSize());
+
+        var game = new GamePanel();
+        game.setSize(window.getSize());
+        game.setLocation(0, 0);
+
+        var sidePanel = new SidePanel();
+        sidePanel.setSize(200, 700);
+        sidePanel.setBackground(new Color(0, 0, 0, 150));
+        sidePanel.setLocation(10, 10);
+
+        layeredPane.add(game, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(sidePanel, JLayeredPane.PALETTE_LAYER);
+
+        window.setContentPane(layeredPane);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        startVirtualThread(game);
+    }
+}
+*/
